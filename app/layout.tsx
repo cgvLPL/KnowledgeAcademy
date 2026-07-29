697f6f206a6dee3b1f5acce3ade233e39801252e
rayhanmawuntu-stack <rayhanmawuntu@gmail.com>
2026-07-29 11:52:16 +0700

    Load certificate presentation

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,6 +39,7 @@ import "./knowledge-academy-loading.css";
 import "./final-loading-viewport-fix.css";
 import "./admin-header-consistency.css";
 import "./brand-visibility-polish.css";
+import "./certificate.css";
 
 const publicSiteUrl = "https://rayhanmawuntu-stack.github.io/CGV.Exams/";
 const socialImageUrl = `${publicSiteUrl}og.png`;
