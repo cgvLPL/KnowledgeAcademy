b263cfce3fd482b8ad25568ce12465194f298810
rayhanmawuntu-stack <rayhanmawuntu@gmail.com>
2026-08-22 22:04:38 +0700

    Load final visual cohesion system

--- app/layout.tsx
+++ app/layout.tsx
@@ -80,6 +80,8 @@ import "./ui-foundation.css";
 import "./certificate-a4-single-page.css";
 import "./live-quiz-monitor.css";
 import "./table-header-visibility-fix.css";
+import "./podium-leaderboard.css";
+import "./visual-cohesion-system.css";
 
 const publicSiteUrl = "https://evalora-quiz.rayhanmawuntu.chatgpt.site/";
 const socialImageUrl = `${publicSiteUrl}og.png`;
