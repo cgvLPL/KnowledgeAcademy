4e48571fc5bb22c40192e8c736d39d0ba5a9b47c
rayhanmawuntu-stack <rayhanmawuntu@gmail.com>
2026-08-22 22:07:19 +0700

    Load admin overview cohesion layer

--- app/layout.tsx
+++ app/layout.tsx
@@ -82,6 +82,7 @@ import "./live-quiz-monitor.css";
 import "./table-header-visibility-fix.css";
 import "./podium-leaderboard.css";
 import "./visual-cohesion-system.css";
+import "./admin-overview-cohesion.css";
 
 const publicSiteUrl = "https://evalora-quiz.rayhanmawuntu.chatgpt.site/";
 const socialImageUrl = `${publicSiteUrl}og.png`;
